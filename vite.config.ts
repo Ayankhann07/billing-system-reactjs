c413b0900cc5a23e49b7dcdd721c80c9f6779767
Ayankhann07 <145081129+Ayankhann07@users.noreply.github.com>
2025-03-08 14:31:04 +0530

    Fixed base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'billing-system-reactjs',  // Ye line add karo
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
